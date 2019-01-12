477e2e42c14079b00a21305aff481b729e8cae3c
Nemin <bergengocia@protonmail.com>
2019-01-13 00:37:08 +0100

    Updated PGYFT database

--- PGYFT/main.js
+++ PGYFT/main.js
@@ -1,32 +1,34 @@
 "use strict"
 
-const filename = "https://nemin32.github.io/PGYFT/datas.json";
+const filename = "https://nemin32.github.io/PGYFT/data.json";
 let root;
 let tag;
 
 window.onload = () => {
     root = document.getElementById("root");
     tag = document.getElementById("tag")
+
+    //root.innerHTML = "<div><h3 style = 'text-align:center'>Welcome! Please select a category to begin.</h3></div>"
     tag.value = ""
 
     fetch(filename).then(resp => resp.json()).then(json => {
         tag.addEventListener("change", () => {
             while (root.firstChild) {root.removeChild(root.firstChild)}
-            for (let i in json) {
+            json["data"].forEach(data => {
                 if (tag.value != "notag") {
-                    if (json[i].tags.indexOf(tag.value) != -1) {
+                    if (data.tags.indexOf(tag.value) != -1) {
                         let elem = document.createElement("div");
-                        elem.innerHTML = `<h3>${json[i].question}</h3><p>${json[i].answer}</p><p class = 'tags'><b>Tags:</b> ${json[i].tags.join(", ")}</p>`
+                        elem.innerHTML = `<h3>${data.question}</h3><p>${data.answer}</p><p class = 'tags'><b>Tags:</b> ${data.tags.join(", ")}</p>`
                         root.appendChild(elem)
                     }
                 } else {
-                    if (json[i].tags.length == 0) {
+                    if (data.tags.length == 0) {
                         let elem = document.createElement("div");
-                        elem.innerHTML = `<h3>${json[i].question}</h3><p>${json[i].answer}</p><p class = 'tags'><b>Tags:</b> None yet...</p>`
+                        elem.innerHTML = `<h3>${data.question}</h3><p>${data.answer}</p><p class = 'tags'><b>Tags:</b> None yet...</p>`
                         root.appendChild(elem)
                     }
                 }
-            }
+            })
         })
     })
 }
