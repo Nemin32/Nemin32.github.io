741358a232bdec211e718913748859c1ee3a3a31
Nemin <pergerlori@gmail.com>
2019-01-16 23:55:26 +0100

    Fully overhauled PGYFT.

--- PGYFT/main.js
+++ PGYFT/main.js
@@ -5,66 +5,50 @@ let root;
 let tag;
 
 function helper(tags) {
-	return tags.map(elem => `<a onclick = "tag.value = '${elem}'; tag.dispatchEvent(new Event('change'))">${elem}</a>`)
+  return tags.map(elem => ` <a class = "tag_link" onclick = "tag.value = '${elem}'; tag.dispatchEvent(new Event('change'))">${elem}</a>`)
+}
+
+function create_div_content(data, no_tags) {
+  return `
+  <p class = "question">${data.question}</p>
+  <p class = "answer">${data.answer}</p>
+  <div class = "bottom">
+    <p class = "source"><a href="${data.link}">Source</a></p>
+    <p class = "tags">Tags: ${(no_tags) ? "None yet..." : helper(data.tags)}</p>
+  </div>
+  `
 }
 
 window.onload = () => {
-    root = document.getElementById("root");
-    tag = document.getElementById("tag")
+  root = document.getElementById("root");
+  tag = document.getElementById("tag")
 
-    //root.innerHTML = "<div><h3 style = 'text-align:center'>Welcome! Please select a category to begin.</h3></div>"
-    tag.value = ""
+  tag.value = ""
 
-    fetch(filename).then(resp => resp.json()).then(json => {
+  fetch(filename).then(resp => resp.json()).then(json => {
+    json["tags"].forEach(tags => {
+      let elem;
+      if (tags.indexOf("-- ") != -1) {
+        elem = document.createElement("optgroup")
+        elem.label = tags;
+      } else {
+        elem = document.createElement("option")
+        elem.innerText = tags;
+      }
 
-        
-        json["tags"].forEach(tags => {
-            let elem;
-            if (tags.indexOf("-- ") != -1) {
-                elem = document.createElement("optgroup")
-                elem.label = tags;
-            } else {
-                elem = document.createElement("option")
-                elem.innerText = tags;
-            }
-
-            elem.value = tags;
-            
-
-            tag.appendChild(elem);
-        })
-
-        tag.addEventListener("change", () => {
-            while (root.firstChild) {root.removeChild(root.firstChild)}
-            json["data"].forEach(data => {
-                if (tag.value != "notag") {
-                    if (data.tags.indexOf(tag.value) != -1) {
-                        let elem = document.createElement("div");
-                        elem.innerHTML = `
-				<h3>${data.question}</h3>
-				<p>${data.answer}</p>
-				<hr>
-				<div class = "flexer">
-					<p class = 'src'><b>Source:</b> <a href = "${data.link}">[Link]</a></p>
-					<p class = 'tags'><b>Tags:</b> ${helper(data.tags)}</p>
-				</div>`
-                        root.appendChild(elem)
-                    }
-                } else {
-                    if (data.tags.length == 0) {
-                        let elem = document.createElement("div");
-                        elem.innerHTML = `
-				<h3>${data.question}</h3>
-				<p>${data.answer}</p>
-				<hr>
-				<div class = "flexer">
-					<p class = 'src'><b>Source:</b> <a href = "${data.link}">[Link]</a></p>
-					<p class = 'tags'><b>Tags:</b> None yet...</p>
-				</div>`
-                        root.appendChild(elem)
-                    }
-                }
-            })
-        })
+      elem.value = tags;
+      tag.appendChild(elem);
     })
+
+    tag.addEventListener("change", () => {
+      while (root.firstChild) {root.removeChild(root.firstChild)}
+      json["data"].forEach(data => {
+        if ((tag.value != "notag" && data.tags.indexOf(tag.value) != -1) || (tag.value == "notag" && data.tags.length == 0)) {
+          let elem = document.createElement("div")
+          elem.innerHTML = create_div_content(data, data.tags.length==0)
+          root.appendChild(elem)
+        }
+      })
+    })
+  })
 }
