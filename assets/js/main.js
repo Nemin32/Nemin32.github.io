6e9d44b1a1a87aeed1887c52b8eecaf0c481f6e1
Nemin <bergengocia@protonmail.com>
2020-01-24 22:30:07 +0100

    Added table of contents

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,23 @@
 let root = document.getElementById("root")
 
-    let elems = []
+let elems = []
 fetch("/assets/json/posts.json").then(e => e.json()).then(json => {
     for (let elem in json) {
         elems.push(elem)
     }
     elems.sort()
 
+    let toc = document.createElement("ul");
+    for (let elem of elems) {
+        let li = document.createElement("li")
+        li.innerHTML = `<a href="#${elem}">${elem}</a>`
+        toc.appendChild(li)
+    }
+    document.getElementById("content").insertBefore(toc, root)
+
     for (let elem of elems) {
         let outer_div = document.createElement("div");
-        outer_div.innerHTML = `<h2>${elem}</h2>`
+        outer_div.innerHTML = `<h2><a id="${elem}"></a>${elem}</h2><center><a href="#toc">Back</a></center><br>`
         
         for (let inner in json[elem]) {
             let div = document.createElement("div");
