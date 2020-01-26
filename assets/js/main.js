2189bd43f836f6704d4a03aca6d8396fc1952205
Nemin <bergengocia@protonmail.com>
2020-01-26 21:36:23 +0100

    Updated PGYFT to include dates

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,21 +18,21 @@ fetch("/assets/json/posts.json").then(e => e.json()).then(json => {
         console.log(toc.value)
         window.location.hash = toc.value
     })
-    
+
     document.getElementById("content").insertBefore(toc, root)
 
     for (let elem of elems) {
         let outer_div = document.createElement("div");
-        outer_div.innerHTML = `<h2><a id="${elem}"></a>${elem}</h2><center><a href="#toc">Back</a></center><br>`
-        
-        for (let inner in json[elem]) {
+        outer_div.innerHTML = `<h2><a id="${elem}"></a>${elem}<br>(Posted on: ${json[elem].month}/${json[elem].year})</h2><center><a href="#toc">Back</a></center><br>`
+
+        for (let inner in json[elem].contents) {
             let div = document.createElement("div");
-            let json_elem = json[elem][inner];
+            let json_elem = json[elem].contents[inner];
 
             div.className = "qna"
             div.innerHTML = `
-<p><strong>${json[elem][inner].question}</strong></p>
-<p>${json[elem][inner].answer}</p>
+<p><strong>${json_elem.question}</strong></p>
+<p>${json_elem.answer}</p>
 `
 
             outer_div.appendChild(div)
