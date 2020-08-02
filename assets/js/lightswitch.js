a07fa31ccc266a2d7fe810fd7d0c8ab1cc01071d
Nemin <bergengocia@protonmail.com>
2020-08-02 23:55:00 +0200

    Better switching

--- assets/js/lightswitch.js
+++ assets/js/lightswitch.js
@@ -1,14 +1,39 @@
 window.addEventListener("load", () => {
   let lightswitch = document.getElementById("lightswitch");
-  let sheet = document.getElementById("style");
+  let sheet = document.createElement("link")
+  sheet.setAttribute("rel", "stylesheet");
+  document.head.appendChild(sheet);
 
-  let light = true;
-  lightswitch.addEventListener("click", () => {
-    if (light)
-      sheet.setAttribute("href", "/css/dark_style.css")
-    else
+  lightswitch.style.visibility = "visible"
+
+  const switchPalette = (light) => {
+    if (light) {
+      document.body.style.backgroundColor = "#e0e0c0";
+      document.querySelector("header").style.backgroundColor = "#3f663b";
       sheet.setAttribute("href", "/css/light_style.css")
+    } else {
+      document.body.style.backgroundColor = "#2e2e2e";
+      document.querySelector("header").style.backgroundColor = "#343";
+      sheet.setAttribute("href", "/css/dark_style.css")
+    }
+  }
+
+  let light = localStorage.getItem("light");
+
+  if (light == null)
+    light = "true";
+
+  light = (light == "true")
+
+  switchPalette(light)
+
+  lightswitch.addEventListener("click", () => {
+    console.log(light)
 
     light = !light
+    switchPalette(light)
+
+    localStorage.setItem("light", light)
   })
 })
+
